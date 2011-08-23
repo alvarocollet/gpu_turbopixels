8bac71cc13066567b1b17669a3421326c816e381
Alvaro Collet <alvaro.collet@gmail.com>
2011-08-22 22:55:32 -0400

    Added copyright notice to headers.

--- src/example.cpp
+++ src/example.cpp
@@ -1,3 +1,41 @@
+/*
+ * example.cpp
+ *
+ *  Created on: Aug 20, 2011
+ *      Author: Alvaro Collet (acollet@cs.cmu.edu)
+ *
+ *  Copyright (c) 2011, Carnegie Mellon University.
+ *  All rights reserved.
+ *
+ * Software License Agreement (BSD License)
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 #include "Turbopixels.h"
 
 /*
@@ -32,7 +70,6 @@ int main(int argc, char** argv){
   std::vector<unsigned int> out(img_size);
   std::vector<unsigned char> input_imgc(img_size);
 
-  cv::flip(img, img, 0);
   // cv::MatIterator_<unsigned char> it = img.begin<unsigned char>();
   // cv::MatIterator_<unsigned char> it_end = img.end<unsigned char>();
   input_imgc.assign( img.begin<unsigned char>(), img.end<unsigned char>() );
@@ -64,9 +101,10 @@ int main(int argc, char** argv){
   for (int i = 0; m < img.end<unsigned char>(); ++i, ++m) 
     *m = shuff[ out[i] % maxVal ];
 
-  cv::flip(img, img, 0);
+  // Write output to file
   cv::imwrite("output.jpg", img);
 
+  // Show output in a window
   cv::namedWindow("superpixels", CV_WINDOW_AUTOSIZE);
   cv::imshow("superpixels", img);
   cv::waitKey();
